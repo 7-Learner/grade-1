86ec97b99f0da938013d43dc55600e5786b0233a
7-Learner <13035959185@163.com>
2025-02-28 22:24:40 +0800

    Add files via upload

---
+++ BMI（亚洲）修正.py
@@ -0,0 +1,17 @@
+height=eval(input("请输入您的身高（米）:"))
+weight=eval(input("请输入您的体重（kg):"))
+BMI=weight/(height**2)
+print("BMI=",BMI)
+print("根据亚洲人的BMI参考标准，您的的身体",end="")
+if BMI<18.5:
+    print("偏瘦")
+elif 18.5<=BMI<23:
+    print("正常")
+elif 23<=BMI<25:
+    print("偏胖")
+elif 25<=BMI<30:
+    print("肥胖")
+elif BMI>=30:
+    print("重度肥胖")
+import time
+time.sleep(35)
